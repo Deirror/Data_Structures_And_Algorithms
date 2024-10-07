e18431fcdd1867050b449fda7e2eebe31868f774
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-07 21:58:36 +0300

    Create LinearSearch.cpp

---
+++ Algorithms/Searching/LinearSearch.cpp
@@ -0,0 +1,9 @@
+template <class T>
+int linearSearch(T* arr, size_t size, const T& searched) {
+    for(int i = 0; i < size; i++) {
+        if(arr[i] == searched) {
+            return i;
+        }  
+    } 
+    return -1;
+}
