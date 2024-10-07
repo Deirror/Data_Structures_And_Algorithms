eff2d67faec0c2d466a9b31529d3a3f7f63bc227
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-07 21:59:53 +0300

    Update LinearSearch.cpp

--- Algorithms/Searching/LinearSearch.cpp
+++ Algorithms/Searching/LinearSearch.cpp
@@ -1,4 +1,4 @@
-//Also could be interpretated as AcceleratedSearch
+//Also could be interpreted as AcceleratedSearch
 template <class T>
 int linearSearch(T* arr, size_t size, const T& searched) {
     for(int i = 0; i < size; i++) {
