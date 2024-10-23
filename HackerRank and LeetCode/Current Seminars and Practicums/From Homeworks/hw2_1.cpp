422c02487277ceb3423c0a00cafa29af0cc40677
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-23 22:25:24 +0300

    Update hw2_1.cpp

--- HackerRank and LeetCode/Current Seminars and Practicums/From Homeworks/hw2_1.cpp
+++ HackerRank and LeetCode/Current Seminars and Practicums/From Homeworks/hw2_1.cpp
@@ -1,51 +1,46 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-long long invited[int(1e5)]{};//max char can be log3(3^33) = 33
+long long invited[int(1e5) + 1]{};
 
-void calc_candys(vector<char>& all, int l, int r, int rec_level = 1) {
-    if(l >= r || rec_level == 32) {//one element, max rec level
-        all[r] = rec_level;
-        return;
+int find_level(long long num, long long max_limit) {
+    int level = 1;
+    long long l = 1, r = max_limit;
+
+    while (l < r) {
+        long long fh_mid = l + (r - l) / 3;
+        long long sc_mid = r - (r - l) / 3;
+
+        if (num > fh_mid && num < sc_mid) {
+            return level;
+        } else if (num <= fh_mid) {
+            r = fh_mid;
+        } else {
+            l = sc_mid;
+        }
+
+        level++;
     }
     
-    int fh_mid = l + (r - l) / 3;
-    int sc_mid = r - (r - l) / 3;    
-    
-    for(int i = fh_mid + 1; i < sc_mid; i++) {
-        all[i] = rec_level;
-    }
-    
-    rec_level++;
-    calc_candys(all, l, fh_mid, rec_level);
-    calc_candys(all, sc_mid, r, rec_level);
+    return level;
 }
-
-vector<char> all;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int p, n;
     cin >> p >> n;
-    
-    for(int i = 0; i < n; i++) {
-        cin >> invited[i];//must be between 1 and 3^p
+
+    long long max_limit = pow(3, p);
+
+    for (int i = 0; i < n; i++) {
+        cin >> invited[i];
     }
-    
-    long long size = pow(3,p);
-    all.resize(size + 1);
-    
-    calc_candys(all, 1, size);
-    
-    for(int i = 0; i < n; i++) {
-        cout << int(all[invited[i]]) << '\n';
+
+    for (int i = 0; i < n; i++) {
+        cout << find_level(invited[i], max_limit) << '\n';
     }
-    
+
     return 0;
 }
