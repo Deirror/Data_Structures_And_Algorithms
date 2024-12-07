596274a574387935e8cb571f6023bb796f42e334
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-12-07 22:51:14 +0200

    Update main.cpp

--- Data Structures/Graphs/main.cpp
+++ Data Structures/Graphs/main.cpp
@@ -2,6 +2,10 @@
 
 int main() {
 	graph<int> gel({ {1,2},{1,3},{1,4},{2,3},{3,4}, {5, 6}, { 4, 6 }, { 2,5 }, {6, 7}, {6, 8}, {8, 9} });
-	cout << gel.path(1, 7);// 3 -> (1,2) - (2,6) - (6,7)
+	cout << gel.path(1, 5);
+	gel.remove({ 1,2 });
+	cout << gel.path(1, 5);
+	gel.add({ 1,2 });
+	cout << gel.path(1, 5);
 	return 0;
 }
