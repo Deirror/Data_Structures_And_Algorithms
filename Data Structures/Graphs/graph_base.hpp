b7a1e05cea73be9417fb49213a5bde42a64d552a
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-12-08 18:13:22 +0200

    Update func connected to is_connected

--- Data Structures/Graphs/graph_base.hpp
+++ Data Structures/Graphs/graph_base.hpp
@@ -15,7 +15,7 @@ public:
 	virtual bool contains(const pair<T, T>& edge) const = 0;
 	virtual bool contains(const T& vertex) const = 0;
 
-	virtual bool connected() const = 0;
+	virtual bool is_connected() const = 0;
 
 	virtual size_t path(const T& startVertex, const T& endVertex) const = 0;
 };
